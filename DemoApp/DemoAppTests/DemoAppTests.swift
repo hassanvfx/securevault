186030aea9516829c53d526725d27cbf7c609513
hassan <hassanvfx@gmail.com>
2024-01-04 23:43:47 -0600

    ready

--- DemoApp/DemoAppTests/DemoAppTests.swift
+++ DemoApp/DemoAppTests/DemoAppTests.swift
@@ -6,29 +6,87 @@
 //
 
 @testable import DemoApp
+import SecureVault
 import XCTest
 
 class DemoAppTests: XCTestCase {
+    struct Token:Codable{
+        var count:Int
+        var expirationDate:Date
+    }
+    var secureVault: SecureVault!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        super.setUp()
+        let uuid = UUID().uuidString
+        secureVault = SecureVault(namespace: uuid)
+       
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        secureVault = nil
+        super.tearDown()
+        // Clean up the mock environment here if necessary
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    
+    func testInitialization() async throws {
+        XCTAssertNotNil(secureVault, "SecureVault should be properly initialized.")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    
+    func testSetAndGet() async throws {
+        let testKey = "testKey"
+        let testValue = "testValue"
+        
+        await secureVault.set(key: testKey, value: testValue)
+        let retrievedValue = await secureVault.get(key: testKey)
+        
+        XCTAssertEqual(retrievedValue, testValue, "The value retrieved should match the value set.")
+    }
+    
+    func testGetWithNonexistentKey() async throws {
+        let retrievedValue = await secureVault.get(key: "nonexistentKey")
+        XCTAssertNil(retrievedValue, "Getting a value for a nonexistent key should return nil.")
+    }
+    
+    func testOverwritingValueForKey() async throws {
+        let testKey = "testKey"
+        let initialValue = "initialValue"
+        let newValue = "newValue"
+        
+        await secureVault.set(key: testKey, value: initialValue)
+        await secureVault.set(key: testKey, value: newValue)
+        let retrievedValue = await secureVault.get(key: testKey)
+        
+        XCTAssertEqual(retrievedValue, newValue, "The new value should overwrite the initial value.")
+    }
+    
+    func testManyValues() async throws {
+        let testKey = "longValue"
+        var item=[Token]()
+        
+        for _ in 0...100000{
+            item.append(Token(count: 1, expirationDate: Date()))
         }
+        
+        let jsonEncoder = JSONEncoder()
+        let data = try jsonEncoder.encode(item)
+        guard let stringData = String(data: data, encoding: .utf8) else {
+            
+            fatalError( "Failure building String out of data")
+        }
+        
+        // Start time
+        let startTime = Date()
+        await secureVault.set(key: testKey, value: stringData)
+        // End time
+        let endTime = Date()
+        // Calculate duration
+        let duration = endTime.timeIntervalSince(startTime)
+        print("write 100_000 tokens took, \(duration)s")
+        
+        let retrievedValue = await secureVault.get(key: testKey)
+        
+        XCTAssertEqual(retrievedValue, stringData, "The new value should overwrite the initial value.")
     }
+    
 }
--- Sources/SecureVault/SecureVault.swift
+++ Sources/SecureVault/SecureVault.swift
@@ -1,1 +1,184 @@
-public struct SecureVault {}
+//
+//  SecureVault.swift
+//
+//  Created by Eon Fluxor on 1/4/24.
+//
+
+import Foundation
+import CryptoKit
+import Security
+
+public class SecureVault {
+    private var namespace="secure"
+    private var encryptionKey: SymmetricKey?
+    private let actor = SecureVaultActor()
+    
+    public init(namespace:String? = nil) {
+        self.namespace = namespace ?? self.namespace
+        if let storedKey = retrieveKeyFromKeychain() {
+            encryptionKey = SymmetricKey(data: storedKey)
+        } else {
+            let newKey = SymmetricKey(size: .bits256)
+            let keyData = newKey.withUnsafeBytes { Data(Array($0)) }
+            storeKeyToKeychain(keyData)
+            encryptionKey = newKey
+        }
+    }
+}
+
+extension SecureVault{
+    struct EncryptedStore: Codable {
+        var store: [String: Data]
+    }
+    
+    enum EncryptionError: Error {
+        case combinedDataNil
+        case encryptionFailed(String)
+    }
+    
+    enum DecryptionError: Error {
+        case invalidData
+        case decryptionFailed(String)
+    }
+    
+    actor SecureVaultActor {
+        private var store: [String: Data] = [:]
+        
+        func set(key: String, value: Data) {
+            store[key] = value
+        }
+        
+        func get(key: String) -> Data? {
+            store[key]
+        }
+    }
+}
+
+public extension SecureVault{
+    func set(key: String, value: String) async {
+        guard let encryptionKey = encryptionKey, let data = value.data(using: .utf8) else {
+            assertionFailure("Encryption key or value is invalid")
+            return
+        }
+        do {
+            let encryptedData = try encrypt(data, using: encryptionKey)
+            await actor.set(key: key, value: encryptedData)
+        } catch {
+            assertionFailure("Encryption failed: \(error)")
+        }
+    }
+    
+    func get(key: String) async -> String? {
+        guard let encryptionKey = encryptionKey else {
+            assertionFailure("Encryption key not found")
+            return nil
+        }
+        do {
+            if let encryptedData = await actor.get(key: key) {
+                let decryptedData = try decrypt(encryptedData, using: encryptionKey)
+                return String(data: decryptedData, encoding: .utf8)
+            }
+            return nil
+        } catch {
+            assertionFailure("Decryption failed: \(error)")
+            return nil
+        }
+    }
+}
+    
+extension SecureVault{
+    private func encrypt(_ data: Data, using key: SymmetricKey) throws -> Data {
+        do {
+            let sealedBox = try AES.GCM.seal(data, using: key)
+            guard let combined = sealedBox.combined else {
+                throw EncryptionError.combinedDataNil
+            }
+            return combined
+        } catch {
+            // Wrap the underlying error into our custom EncryptionError
+            throw EncryptionError.encryptionFailed(error.localizedDescription)
+        }
+    }
+    
+    private func decrypt(_ data: Data, using key: SymmetricKey) throws -> Data {
+        do {
+            let box = try AES.GCM.SealedBox(combined: data)
+            return try AES.GCM.open(box, using: key)
+        } catch {
+            // Wrap the underlying error into our custom DecryptionError
+            throw DecryptionError.decryptionFailed(error.localizedDescription)
+        }
+    }
+    
+    private var secureFilePath:URL{
+        getDocumentsDirectory().appendingPathComponent(secureFilename)
+    }
+    
+    private var keychainKey:String{
+        "com.secureVault.\(namespace)"
+    }
+    
+    private var secureFilename:String{
+        "database.db"
+    }
+    
+    private func storeKeyToKeychain(_ data: Data) {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrApplicationTag as String: keychainKey,
+            kSecValueData as String: data
+        ]
+        SecItemAdd(query as CFDictionary, nil)
+    }
+    
+    private func retrieveKeyFromKeychain() -> Data? {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassKey,
+            kSecAttrApplicationTag as String: keychainKey,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+        var item: CFTypeRef?
+        if SecItemCopyMatching(query as CFDictionary, &item) == noErr {
+            return (item as? Data)
+        }
+        return nil
+    }
+    
+    private func dataStore()->EncryptedStore{
+        let filePath = secureFilePath
+        do {
+            let data = try Data(contentsOf: filePath)
+            let store = try JSONDecoder().decode(EncryptedStore.self, from: data)
+            return store
+        } catch {
+            print("Failed to red from disk: \(error)")
+            return EncryptedStore(store:[:])
+        }
+    }
+    
+    @discardableResult
+    private func write(key: String, data: Data) async -> Bool {
+        let filePath = secureFilePath
+        var dataStore = dataStore()
+        dataStore.store[key] = data
+        do {
+            let encodedData = try JSONEncoder().encode(dataStore)
+            try encodedData.write(to: filePath)
+            return true
+        } catch {
+            print("Failed to write to disk: \(error)")
+            return false
+        }
+    }
+    
+    private func read(key: String) async -> Data? {
+        dataStore().store[key]
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
+    }
+}
+
